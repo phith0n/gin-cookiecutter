6582918cc643535f031c21cfaa3b6e6c3f742a4e
phith0n <phith0n.ph2f@gmail.com>
2023-05-20 00:46:31 +0800

    fix database error on MySQL

--- {{cookiecutter.module_name}}/db/base.go
+++ {{cookiecutter.module_name}}/db/base.go
@@ -4,6 +4,6 @@ import "time"
 
 type BaseModel struct {
 	ID             uint      `json:"id" gorm:"column:id;not null;primaryKey;autoIncrement;"`
-	CreatedTime    time.Time `json:"created_time" gorm:"column:created_time;type:timestamptz(6);autoCreateTime;"`
-	LastModifyTime time.Time `json:"last_modify_time" gorm:"column:last_modify_time;type:timestamptz(6);autoUpdateTime;"`
+	CreatedTime    time.Time `json:"created_time" gorm:"column:created_time;autoCreateTime;"`
+	LastModifyTime time.Time `json:"last_modify_time" gorm:"column:last_modify_time;autoUpdateTime;"`
 }
